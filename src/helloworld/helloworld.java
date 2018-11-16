294494b18683378790a3b826252e9d848e195ba5
singvey999 <38499013@qq.com>
2018-11-16 14:12:03 +0800

    11/18 8:48 submit

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -5,6 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world with v1 version");
+		System.out.println("hello world with v201811161208 version");
 
 	}
 
